b899ab85e9df7fab24003aa6a84839790f8678d7
Vsomera <vsomera77@outlook.com>
2024-08-31 22:31:23 -0700

    refactor

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,31 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/vsomera/focusmode/hosts"
 )
+
+var store = hosts.NewHostsStore()
+
+var rootCmd = &cobra.Command{
+	Use:   "fmode",
+	Short: `Cli tool to block distracting websites`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Print(`FocusMode Cli Tool`)
+	},
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: `Lists all domains that are currently being blocked`,
+	Run: func(cmd *cobra.Command, args []string) {
+		domains, err := store.GetDomainsFromHost()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Printf(`List of blocked domains: %v`, domains)
+	},
+}
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
@@ -14,10 +38,6 @@ func Execute() {
 	}
 }
 
-var rootCmd = &cobra.Command{
-	Use:   "fmode",
-	Short: `Cli tool to block distracting websites`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print(`FocusMode Cli Tool`)
-	},
+func init() {
+	rootCmd.AddCommand(listCmd)
 }
