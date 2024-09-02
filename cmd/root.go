3bf20d20a64f6baea2a89237db238d956d52d5ba
Vsomera <vsomera77@outlook.com>
2024-09-01 20:49:49 -0700

    basic commands

--- cmd/root.go
+++ cmd/root.go
@@ -8,13 +8,15 @@ import (
 	"github.com/vsomera/focusmode/hosts"
 )
 
+// TODO : implement cleaning domains, and selecting domains to delete
+
 var store = hosts.NewHostsStore()
 
 var rootCmd = &cobra.Command{
 	Use:   "fmode",
 	Short: `Cli tool to block distracting websites`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print(`FocusMode Cli Tool`)
+		fmt.Print("\nFocusMode\n\n| Cli Tool to block distracting websites, focus on what actually matters.\n\n")
 	},
 }
 
@@ -27,7 +29,43 @@ var listCmd = &cobra.Command{
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Printf(`List of blocked domains: %v`, domains)
+
+		fmt.Print("\nBlacklist:\n\n")
+		for i, d := range domains {
+			fmt.Printf("|  %v %s\n", i+1, d)
+		}
+		fmt.Println("")
+
+	},
+}
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: `Add domain(s) to block list`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Print("\nError:\n\n|  No domain arguments in command call\n\n")
+			return
+		}
+
+		existingDomains, err := store.GetDomainsFromHost()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		domainsToAdd := append(existingDomains, args...)
+		err = store.AddDomainsToHost(domainsToAdd)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Print("\nAdded domain(s) to Blacklist:\n\n")
+		for i, d := range args {
+			fmt.Printf("|  %v %s\n", i+1, d)
+		}
+		fmt.Println("")
+
 	},
 }
 
@@ -40,5 +78,5 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(listCmd, addCmd)
 }
