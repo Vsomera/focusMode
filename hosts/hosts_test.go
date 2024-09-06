34f1af457bba67822d78a9e0a715b756109cf9c3
Vsomera <vsomera77@outlook.com>
2024-09-05 19:53:44 -0700

    refactor

--- hosts/hosts_test.go
+++ hosts/hosts_test.go
@@ -18,30 +18,18 @@ func TestGetDomainsFromHost(t *testing.T) {
 		expectedData []string
 	}{
 		{
-			name: "get domains within markers",
-			initialData: `
-			0.0.0.0 www.youtube.com
-			#focusmode:start
-			127.0.0.1 www.instagram.com
-			127.0.0.1 www.facebook.com
-			#focusmode:end
-			`,
+			name:         "get domains within markers",
+			initialData:  "0.0.0.0 www.youtube.com\n#focusmode:start\n127.0.0.1 www.instagram.com\n127.0.0.1 www.facebook.com\n#focusmode:end",
 			expectedData: []string{"www.instagram.com", "www.facebook.com"},
 		},
 		{
-			name: "no domains within markers",
-			initialData: `
-			127.0.0.1 www.google.com
-			#focusmode:start
-			#focusmode:end
-			`,
+			name:         "no domains within markers",
+			initialData:  "127.0.0.1 www.google.com\n#focusmode:start\n#focusmode:end",
 			expectedData: []string{},
 		},
 		{
-			name: "no markers present",
-			initialData: `
-        	127.0.0.1 www.google.com
-			`,
+			name:         "no markers present",
+			initialData:  "127.0.0.1 www.google.com",
 			expectedData: []string{},
 		},
 	}
@@ -72,45 +60,22 @@ func TestAddDomainsToHost(t *testing.T) {
 		expectedDomains  []string
 	}{
 		{
-			name:            "adding domains to empty hosts file",
-			initialData:     ``,
-			expectedDomains: []string{"www.instagram.com", "www.youtube.com"},
-			expectedFileData: `#focusmode:start
-127.0.0.1 www.instagram.com
-127.0.0.1 www.youtube.com
-#focusmode:end`,
+			name:             "adding domains to empty hosts file",
+			initialData:      ``,
+			expectedDomains:  []string{"www.instagram.com", "www.youtube.com"},
+			expectedFileData: "#focusmode:start\n127.0.0.1 www.instagram.com\n127.0.0.1 www.youtube.com\n#focusmode:end",
 		},
 		{
-			name: "overwriting domains to existing hosts file",
-			initialData: `#focusmode:start
-127.0.0.1 www.youtube.com
-127.0.0.1 www.instagram.com
-127.0.0.1 www.github.com
-#focusmode:end`,
-			expectedDomains: []string{"www.youtube.com", "www.facebook.com", "www.amazon.com"},
-			expectedFileData: `#focusmode:start
-127.0.0.1 www.youtube.com
-127.0.0.1 www.facebook.com
-127.0.0.1 www.amazon.com
-#focusmode:end`,
+			name:             "overwriting domains to existing hosts file",
+			initialData:      "#focusmode:start\n127.0.0.1 www.youtube.com\n127.0.0.1 www.instagram.com\n127.0.0.1 www.github.com\n#focusmode:end",
+			expectedDomains:  []string{"www.youtube.com", "www.facebook.com", "www.amazon.com"},
+			expectedFileData: "#focusmode:start\n127.0.0.1 www.youtube.com\n127.0.0.1 www.facebook.com\n127.0.0.1 www.amazon.com\n#focusmode:end",
 		},
 		{
-			name: "overwriting domains to hosts file without deleting contents",
-			initialData: `# comment
-#comment
-#focusmode:start
-127.0.0.1 www.youtube.com
-127.0.0.1 www.instagram.com
-127.0.0.1 www.github.com
-#focusmode:end`,
-			expectedDomains: []string{"www.youtube.com", "www.facebook.com", "www.amazon.com"},
-			expectedFileData: `# comment
-#comment
-#focusmode:start
-127.0.0.1 www.youtube.com
-127.0.0.1 www.facebook.com
-127.0.0.1 www.amazon.com
-#focusmode:end`,
+			name:             "overwriting domains to hosts file without deleting contents",
+			initialData:      "# comment\n#comment\n#focusmode:start\n127.0.0.1 www.youtube.com\n127.0.0.1 www.instagram.com\n127.0.0.1 www.github.com\n#focusmode:end",
+			expectedDomains:  []string{"www.youtube.com", "www.facebook.com", "www.amazon.com"},
+			expectedFileData: "# comment\n#comment\n#focusmode:start\n127.0.0.1 www.youtube.com\n127.0.0.1 www.facebook.com\n127.0.0.1 www.amazon.com\n#focusmode:end",
 		},
 	}
 
@@ -146,44 +111,22 @@ func TestCleanDomainsFromHost(t *testing.T) {
 		expectedDomains  []string
 	}{
 		{
-			name: "delete all domains in host",
-			initialData: `# comment
-#comment
-#focusmode:start
-127.0.0.1 www.youtube.com
-127.0.0.1 www.instagram.com
-127.0.0.1 www.github.com
-#focusmode:end`,
-			expectedFileData: `# comment
-#comment
-#focusmode:start
-#focusmode:end`,
-			expectedDomains: []string{},
+			name:             "delete all domains in host",
+			initialData:      "# comment\n#comment\n#focusmode:start\n127.0.0.1 www.youtube.com\n127.0.0.1 www.instagram.com\n127.0.0.1 www.github.com\n#focusmode:end",
+			expectedFileData: "# comment\n#comment\n#focusmode:start\n#focusmode:end",
+			expectedDomains:  []string{},
 		},
 		{
-			name: "empty hosts markers",
-			initialData: `127.0.0.1 www.instagram.com
-# 0.0.0.0 www.docker.com
-#focusmode:start
-#focusmode:end`,
-			expectedFileData: `127.0.0.1 www.instagram.com
-# 0.0.0.0 www.docker.com
-#focusmode:start
-#focusmode:end`,
-			expectedDomains: []string{},
+			name:             "empty hosts markers",
+			initialData:      "127.0.0.1 www.instagram.com\n# 0.0.0.0 www.docker.com\n#focusmode:start\n#focusmode:end",
+			expectedFileData: "127.0.0.1 www.instagram.com\n# 0.0.0.0 www.docker.com\n#focusmode:start\n#focusmode:end",
+			expectedDomains:  []string{},
 		},
 		{
-			name: "empty hosts markers with stub domains",
-			initialData: `127.0.0.1 www.instagram.com
-# 0.0.0.0 www.docker.com
-#focusmode:start
-0.0.0.0 www.youtube.com
-#focusmode:end`,
-			expectedFileData: `127.0.0.1 www.instagram.com
-# 0.0.0.0 www.docker.com
-#focusmode:start
-#focusmode:end`,
-			expectedDomains: []string{},
+			name:             "empty hosts markers with stub domains",
+			initialData:      "127.0.0.1 www.instagram.com\n# 0.0.0.0 www.docker.com\n#focusmode:start\n0.0.0.0 www.youtube.com\n#focusmode:end",
+			expectedFileData: "127.0.0.1 www.instagram.com\n# 0.0.0.0 www.docker.com\n#focusmode:start\n#focusmode:end",
+			expectedDomains:  []string{},
 		},
 	}
 
@@ -207,6 +150,62 @@ func TestCleanDomainsFromHost(t *testing.T) {
 			assertDomains(t, got, tt.expectedDomains)
 			assertFileData(t, tempHostsFile, tt.expectedFileData)
 		})
+	}
+}
+
+func TestDeleteDomainFromHost(t *testing.T) {
+	tests := []struct {
+		name             string
+		domainToDelete   string
+		initialData      string
+		expectedFileData string
+		expectedDomains  []string
+		expectedErr      error
+	}{
+		{
+			name:             "empty domain list",
+			domainToDelete:   "www.instagram.com",
+			initialData:      ``,
+			expectedFileData: ``,
+			expectedDomains:  []string{},
+			expectedErr:      errNoDomains,
+		},
+		{
+			name:             "remove instagram.com",
+			domainToDelete:   "www.instagram.com",
+			initialData:      "#focusmode:start\n127.0.0.1 www.instagram.com\n127.0.0.1 www.facebook.com\n127.0.0.1 www.snapchat.com\n#focusmode:end",
+			expectedFileData: "#focusmode:start\n127.0.0.1 www.facebook.com\n127.0.0.1 www.snapchat.com\n#focusmode:end",
+			expectedDomains:  []string{"www.facebook.com", "www.snapchat.com"},
+			expectedErr:      nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tempHostsFile, cleanFile := createTempFile(t, tt.initialData)
+			defer cleanFile()
+
+			store := &HostsStore{hostsFile: tempHostsFile}
+			defer store.Close()
+
+			err := store.DeleteDomainFromHost(tt.domainToDelete)
+			assertError(t, err, tt.expectedErr)
+
+			d, err := store.GetDomainsFromHost()
+			if err != nil {
+				t.Errorf("GetDomainsFromHost() error: %v", err)
+			}
+
+			assertDomains(t, d, tt.expectedDomains)
+			assertFileData(t, tempHostsFile, tt.expectedFileData)
+		})
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Wrong error type got: %v, want: %v", got, want)
 	}
 }
 
